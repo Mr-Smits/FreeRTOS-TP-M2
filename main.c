1f3aa091e4b80de09a5307fb5fb6f2ad1bedf5c3
smits <stan.smits.25@gmail.com>
2025-01-16 09:23:17 +0100

    fix demultiplexage + modif print debug

--- main.c
+++ main.c
@@ -10,17 +10,22 @@ int main(void) {
         long_frame_t frame = {0};
         frame = generate_frames();
 
-        for(int ii = 0; ii < TAILLE_TABLEAU ; ii++) printf("%02X", frame.frame[ii]);
+        for(int ii = 0; ii < TAILLE_TABLEAU; ii++) printf("%02X", frame.frame[ii]);
         printf("\n");
 
         valeurs_voies_t valeurs = {0};
         valeurs = demultiplexage(frame);
         
-        for(int ii = 0; ii < BYTES_PER_VALUE; ii++) printf("Voie1 : 0x%02X",valeurs.voie1.valeur[ii]);
+        //print for demo purpose
+        printf("horodatage = %ld\n",valeurs.voie1.horodatage);
+        printf("Voie1 : ");
+        for(int ii = 0; ii < BYTES_PER_VALUE; ii++) printf("%02X",valeurs.voie1.valeur[ii]);
         printf("\n");
-        for(int ii = 0; ii < BYTES_PER_VALUE; ii++) printf("Voie2 : 0x%02X",valeurs.voie2.valeur[ii]);
+        printf("Voie2 : ");
+        for(int ii = 0; ii < BYTES_PER_VALUE; ii++) printf("%02X",valeurs.voie2.valeur[ii]);
         printf("\n");
-        for(int ii = 0; ii < BYTES_PER_VALUE; ii++) printf("Voie3 : 0x%02X",valeurs.voie3.valeur[ii]);
+        printf("Voie3 : ");
+        for(int ii = 0; ii < BYTES_PER_VALUE; ii++) printf("%02X",valeurs.voie3.valeur[ii]);
         printf("\n\n");
 
         sleep(1);
